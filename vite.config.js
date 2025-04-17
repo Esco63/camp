240c9298f4742d8e550e0be773a65c82bd04ee8b
Esco63 <andre.michel1994@gmail.com>
2025-04-17 14:43:07 +0200

    Seiten Update

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,17 @@ import { defineConfig } from 'vite'
 import { resolve } from 'path'
 
 export default defineConfig({
+  server: {
+    host: true // Damit dein Handy im WLAN die Seite sehen kann
+  },
   build: {
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
         buchung: resolve(__dirname, 'buchung.html'),
-        geraete: resolve(__dirname, "geraete.html")  // <== das hier ist wichtig
+        geraete: resolve(__dirname, "geraete.html")
       }
     }
   }
 })
+
